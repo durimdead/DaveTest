1ac28da383e49c6b34e18242d04a0e328f40e5d4
david.lancellotti <david.lancellotti@gmail.com>
2022-09-17 12:45:05 -0400

    - added interface for user - put back initialized forecast for weather page - updated main page to be a user listing with just title

---
+++ DaveTest/Data/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace DaveTest.Data
+{
+    public interface IUser
+    {
+        public bool CreateUser(string userName, string address, string phoneNumber, int age);
+        public bool DeleteUser(int userID);
+        public bool EditUser(string userName, string address, string phoneNumber, int age, int userID);
+    }
+}
