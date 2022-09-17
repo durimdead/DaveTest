9833bb491a916583617421c89f71ce1716b0b990
david <david@DESKTOP-UH0MAM2>
2022-09-17 12:17:44 -0400

    Add project files.

---
+++ DaveTest/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+namespace DaveTest.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+
+        public int weatherID { get; set; }
+    }
+}
---
+++ DaveTest/Data/WeatherForecastService.cs
@@ -0,0 +1,31 @@
+namespace DaveTest.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        public WeatherForecast[] GetForecast(DateTime startDate)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                weatherID = index
+            }).ToArray();
+        }
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
