449312f18a33f79a9319d3423b8f8a1d00211244
david.lancellotti <david.lancellotti@gmail.com>
2022-09-17 14:23:12 -0400

    updated User interface and repo

--- DaveTest/Data/Repositories/UserRepository.cs
+++ DaveTest/Data/Repositories/UserRepository.cs
@@ -1,6 +1,7 @@
-﻿namespace DaveTest.Data.Interfaces
+﻿namespace DaveTest.Data.Repositories
 {
-    public interface IUser
+    using DaveTest.Data.Interfaces;
+    public class UserRepository : IUser
     {
         /// <summary>
         /// Used to create a new user in the database.
@@ -11,14 +12,38 @@
         /// <param name="phoneNumber">phone number for the user</param>
         /// <param name="age">user's age</param>
         /// <returns>true if successful creation</returns>
-        public bool CreateUser(string FirstName, string LastName, string address, string phoneNumber, int age);
+        public bool CreateUser(string FirstName, string LastName, string address, string phoneNumber, int age)
+        {
+            bool returnValue = true;
+            try
+            {
+
+            }
+            catch(Exception e)
+            {
+                returnValue = false;
+            }
+            return returnValue;
+        }
 
         /// <summary>
         /// delete the user specified based on the ID sent through.
         /// </summary>
         /// <param name="userID"></param>
         /// <returns>true if successful delete</returns>
-        public bool DeleteUser(int userID);
+        public bool DeleteUser(int userID)
+        {
+            bool returnValue = true;
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                returnValue = false;
+            }
+            return returnValue;
+        }
 
         /// <summary>
         /// Update the user based on the information sent in
@@ -30,6 +55,18 @@
         /// <param name="age">user's age</param>
         /// <param name="userID">ID of the user record</param>
         /// <returns>true if edit is successful</returns>
-        public bool EditUser(string FirstName, string LastName, string address, string phoneNumber, int age, int userID);
+        public bool EditUser(string FirstName, string LastName, string address, string phoneNumber, int age, int userID)
+        {
+            bool returnValue = true;
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                returnValue = false;
+            }
+            return returnValue;
+        }
     }
 }
